e7ca11ee4b368050949e3e65da96303ff76ebf52
shahriarb <shahriar.b@gmail.com>
2018-02-12 14:49:56 +0000

    check exclude_list before adding to deployment list

--- bin/exclude_list.rb
+++
@@ -1,8 +0,0 @@
-module Deployment
-  def exclude_list
-    @result = []
-    exclude_file = File.join("config", "deploy", "exclude_list")
-    File.readlines(exclude_file).each { |line| @result << line.strip } if File.exist?(exclude_file)
-    @result.reject { |r| r.empty? }
-  end
-end
